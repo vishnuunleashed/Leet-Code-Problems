1ae95465b981a579c7df9a39c899854ee1f429e1
Vishnuprasad T R <63510296+vishnuunleashed@users.noreply.github.com>
2023-12-05 15:03:40 +0530

    Add files via upload

---
+++ number_pattern5.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+void main() {
+  // String name = stdin.readLineSync()!;
+  // int limit = int.parse(name);
+
+  /* 
+    print the following pattern
+            1 
+          2 1 2 
+        2 3 1 2 3 
+      2 3 4 1 2 3 4 
+    2 3 4 5 1 2 3 4 5
+    
+  */
+  int limit = 6;
+for (int i = 0; i < limit; i++) {
+    String stars = '';
+    for (int k = i; k < limit; k++) {
+      stars += '  ';
+    }
+    for (int j = i; j > 0; j--) {
+     
+      stars += '$j ';
+    }
+    for (int k = limit; k > i; k--) {
+      stars += '';
+    }
+    for (int j = 2; j <= i; j++) {
+      stars += '$j ';
+    }
+    print(stars);
+  }
+}
